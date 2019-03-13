543b877b6b3d4d53b9e294cf4e233257352f8e8b
Kerem Sevencan <ciukerem@gmail.com>
2019-03-13 17:24:05 +0100

    Updates

---
+++ precache-manifest.43331e97b4ece7e5d261d0be673e884c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "df2c0b08f024607e9f30a17918dc24f3",
+    "url": "/champions-list/static/media/roboto-regular-webfont.df2c0b08.woff2"
+  },
+  {
+    "revision": "78dbe102cfa38fdb6ab5",
+    "url": "/champions-list/static/css/main.82df3c71.chunk.css"
+  },
+  {
+    "revision": "54d5b13aa1f1ea26a318",
+    "url": "/champions-list/static/js/runtime~main.96c33f28.js"
+  },
+  {
+    "revision": "9252f94138a931c8949be34cd23f6fda",
+    "url": "/champions-list/static/media/roboto-mediumitalic-webfont.9252f941.woff"
+  },
+  {
+    "revision": "157a6e0cc0d0acb344ba",
+    "url": "/champions-list/static/js/2.863837aa.chunk.js"
+  },
+  {
+    "revision": "14b094828bfacd0c63f1108a3717345b",
+    "url": "/champions-list/static/media/roboto-medium-webfont.14b09482.woff2"
+  },
+  {
+    "revision": "23a4654b068f497ee45e8104786af250",
+    "url": "/champions-list/static/media/roboto-light-webfont.23a4654b.woff2"
+  },
+  {
+    "revision": "78dbe102cfa38fdb6ab5",
+    "url": "/champions-list/static/js/main.6713beb3.chunk.js"
+  },
+  {
+    "revision": "f4bab878ed816e54f221894df6427202",
+    "url": "/champions-list/static/media/roboto-mediumitalic-webfont.f4bab878.woff2"
+  },
+  {
+    "revision": "e9cb17d19f7c4242fed2ae3ac8fc3312",
+    "url": "/champions-list/static/media/roboto-light-webfont.e9cb17d1.woff"
+  },
+  {
+    "revision": "10f738add54c1e4aaf08bc36f2542cc7",
+    "url": "/champions-list/static/media/roboto-medium-webfont.10f738ad.woff"
+  },
+  {
+    "revision": "2742bac41590c836f5ac64122bf4295e",
+    "url": "/champions-list/static/media/roboto-regular-webfont.2742bac4.woff"
+  },
+  {
+    "revision": "157a6e0cc0d0acb344ba",
+    "url": "/champions-list/static/css/2.6049c853.chunk.css"
+  },
+  {
+    "revision": "33777bb14a5051eab7ff061b96e2d7ed",
+    "url": "/champions-list/index.html"
+  }
+];
